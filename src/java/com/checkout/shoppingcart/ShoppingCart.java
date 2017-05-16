184ee61824882805fad7a0f48fb403b809c1bb0a
mplusf <minal.fulzele@yahoo.com>
2017-05-16 10:01:06 +0100

    green - GIVEN_2apples_THEN_totalcost60p

--- src/java/com/checkout/shoppingcart/ShoppingCart.java
+++ src/java/com/checkout/shoppingcart/ShoppingCart.java
@@ -20,9 +20,18 @@ public class ShoppingCart {
 
     public BigDecimal getTotalBill() {
         BigDecimal totalBills  = new BigDecimal("0.00");
+        int countApple =0;
+        int countOranges = 0;
         for(Fruit fruit : fruits) {
-            totalBills = totalBills.add(fruit.cost);
+            if(fruit.equals(Fruit.APPLE)) countApple++;
+            if(fruit.equals(Fruit.ORANGE)) countOranges++;
         }
+        int appletodiscount = countApple / 2;
+        int totalApplestoCharge = appletodiscount + (countApple%2);
+
+        BigDecimal totalAppleCharges = new BigDecimal(Integer.valueOf(totalApplestoCharge)).multiply(Fruit.APPLE.cost);
+        totalBills = totalBills.add(totalAppleCharges);
+
         return totalBills;
     }
 }
