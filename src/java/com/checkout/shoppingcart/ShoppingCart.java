679b647ce1c1a71b65f1b27c26c538559bbafc61
mplusf <minal.fulzele@yahoo.com>
2017-05-16 09:39:38 +0100

    next red test -- GIVEN_1orangeAnd1Apple_THEN_totalcost85p

--- src/java/com/checkout/shoppingcart/ShoppingCart.java
+++ src/java/com/checkout/shoppingcart/ShoppingCart.java
@@ -1,9 +1,11 @@
 package com.checkout.shoppingcart;
 
 
+import java.util.List;
+
 public class ShoppingCart {
-    public int getTotalBill(String fruit) {
-        if(fruit.equalsIgnoreCase("Apple")) {
+    public int getTotalBill(List<String> fruit) {
+        if(fruit.get(0).equalsIgnoreCase("Apple")) {
             return 60;
         } else {
             return 25;
--- src/test/com/checkout/shoppingcart/ShoppingCartTest.java
+++ src/test/com/checkout/shoppingcart/ShoppingCartTest.java
@@ -2,18 +2,34 @@ package com.checkout.shoppingcart;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCartTest {
 
 
     @Test
     public void GIVEN_1Apple_THEN_totalcost60p() {
         ShoppingCart shoppingCart = new ShoppingCart();
-        assert shoppingCart.getTotalBill("Apple") == 60;
+        List<String> shoppingBasket = new ArrayList<>();
+        shoppingBasket.add("Apple");
+        assert shoppingCart.getTotalBill(shoppingBasket) == 60;
     }
 
     @Test
     public void GIVEN_1orange_THEN_totalcost25p() {
         ShoppingCart shoppingCart = new ShoppingCart();
-        assert shoppingCart.getTotalBill("Orange") == 25;
+        List<String> shoppingBasket = new ArrayList<>();
+        shoppingBasket.add("Orange");
+        assert shoppingCart.getTotalBill(shoppingBasket) == 25;
+    }
+
+    @Test
+    public void GIVEN_1orangeAnd1Apple_THEN_totalcost85p() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        List<String> shoppingBasket = new ArrayList<>();
+        shoppingBasket.add("Apple");
+        shoppingBasket.add("Orange");
+        assert shoppingCart.getTotalBill(shoppingBasket) == 85;
     }
 }
