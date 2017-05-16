48c0c0f89b93912d97abdbca4525c9a53b5f3238
mplusf <minal.fulzele@yahoo.com>
2017-05-16 10:08:49 +0100

    new test case -- GIVEN_4apples_THEN_totalcost1pound20pence -- already green

--- src/test/com/checkout/shoppingcart/OfferTest.java
+++ src/test/com/checkout/shoppingcart/OfferTest.java
@@ -49,6 +49,12 @@ public class OfferTest {
         assert totalbill.equals(new BigDecimal("1.00"));
     }
 
+    @Test
+    public void GIVEN_4apples_THEN_totalcost1pound20pence() {
+        ShoppingCart shoppingCart = new ShoppingCart(createShoppingBasketItems(APPLE,APPLE,APPLE,APPLE));
+        BigDecimal totalbill = shoppingCart.getTotalBill();
+        assert totalbill.equals(new BigDecimal("1.20"));
+    }
 
     private List<ShoppingCart.Fruit> createShoppingBasketItems(ShoppingCart.Fruit... items) {
         return Arrays.asList(items);
