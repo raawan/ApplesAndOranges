70265cdd877f11a08e2ea66d6cad545259501037
mplusf <minal.fulzele@yahoo.com>
2017-05-16 09:42:48 +0100

    refactored test class to use common method to create shopping basket …

--- src/test/com/checkout/shoppingcart/ShoppingCartTest.java
+++ src/test/com/checkout/shoppingcart/ShoppingCartTest.java
@@ -2,34 +2,32 @@ package com.checkout.shoppingcart;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ShoppingCartTest {
 
 
+
     @Test
     public void GIVEN_1Apple_THEN_totalcost60p() {
         ShoppingCart shoppingCart = new ShoppingCart();
-        List<String> shoppingBasket = new ArrayList<>();
-        shoppingBasket.add("Apple");
-        assert shoppingCart.getTotalBill(shoppingBasket) == 60;
+        assert shoppingCart.getTotalBill(createShoppingBasketItems("Apple")) == 60;
     }
 
     @Test
     public void GIVEN_1orange_THEN_totalcost25p() {
         ShoppingCart shoppingCart = new ShoppingCart();
-        List<String> shoppingBasket = new ArrayList<>();
-        shoppingBasket.add("Orange");
-        assert shoppingCart.getTotalBill(shoppingBasket) == 25;
+        assert shoppingCart.getTotalBill(createShoppingBasketItems("Orange")) == 25;
     }
 
     @Test
     public void GIVEN_1orangeAnd1Apple_THEN_totalcost85p() {
         ShoppingCart shoppingCart = new ShoppingCart();
-        List<String> shoppingBasket = new ArrayList<>();
-        shoppingBasket.add("Apple");
-        shoppingBasket.add("Orange");
-        assert shoppingCart.getTotalBill(shoppingBasket) == 85;
+        assert shoppingCart.getTotalBill(createShoppingBasketItems("Apple","Orange")) == 85;
+    }
+
+    private List<String> createShoppingBasketItems(String... items) {
+        return Arrays.asList(items);
     }
 }
